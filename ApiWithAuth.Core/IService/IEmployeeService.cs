48a7995be4fd7e1da066038627bf2b0aa9335656
Muhydeen Adediran <50783764+adediranmuhydeen@users.noreply.github.com>
2023-01-12 00:55:13 +0100

    Employee controller implemented and all endpoints created with local exceptions handled

--- ApiWithAuth.Core/IService/IEmployeeService.cs
+++ ApiWithAuth.Core/IService/IEmployeeService.cs
@@ -7,14 +7,14 @@ namespace ApiWithAuth.Core.IService
 {
     public interface IEmployeeService
     {
-        Task<Response<Employee>> GetEmployeeByIdAsync(Guid id);
-        Task<Response<Employee>> GetEmployeeAsync(Expression<Func<Employee, bool>> exp, List<string> include = null);
-        Task<Response<IEnumerable<Employee>>> GetEmployeeAllAsync();
-        Task<Response<IEnumerable<Employee>>> GetEmployeeAllAsync(Expression<Func<Employee, bool>> predicate, Func<IQueryable<Employee>, IOrderedQueryable<Employee>> orderBy = null, List<string> include = null);
-        Task<Response<Employee>> UpdateEmployeeAsync(Guid id, UpdateEmployeeDto dto);
-        Task<Response<Employee>> DeletEmployeeAsync(Guid id);
+        Task<Response<GetEmployeeDto>> GetEmployeeByIdAsync(Guid id);
+        Task<Response<GetEmployeeDto>> GetEmployeeAsync(Expression<Func<Employee, bool>> exp, List<string> include = null);
+        Task<Response<IEnumerable<GetEmployeeDto>>> GetEmployeeAllAsync();
+        Task<Response<IEnumerable<GetEmployeeDto>>> GetEmployeeAllAsync(Expression<Func<Employee, bool>> predicate, Func<IQueryable<Employee>, IOrderedQueryable<Employee>> orderBy = null, List<string> include = null);
+        Task<Response<GetEmployeeDto>> UpdateEmployeeAsync(Guid id, UpdateEmployeeDto dto);
+        Task<Response<GetEmployeeDto>> DeletEmployeeAsync(Guid id);
         Task<Response<GetEmployeeDto>> DeletEmployeeAsync(string email);
-        Task<Response<Employee>> AddEmployeeAsync(AddEmployeeDto dto);
+        Task<Response<GetEmployeeDto>> AddEmployeeAsync(AddEmployeeDto dto);
         Task<Response<GetEmployeeDto>> GetEmployeeByPhoneNumber(string pnoneNumber);
         Task<Response<GetEmployeeDto>> GetEmployeeByEmail(string email);
 
--- ApiWithAuth.Services/EmployeeS/EmployeeService.cs
+++ ApiWithAuth.Services/EmployeeS/EmployeeService.cs
@@ -19,55 +19,61 @@ namespace ApiWithAuth.Services.EmployeeS
             _mapper = mapper;
         }
 
-        public async Task<Response<Employee>> AddEmployeeAsync(AddEmployeeDto dto)
+        public async Task<Response<GetEmployeeDto>> AddEmployeeAsync(AddEmployeeDto dto)
         {
+            var checkEmployeeEmail = await _unitOfWork.employeeRepository.GetAsync(x => x.Email == dto.Email);
+            var checkEmployeePhone = await _unitOfWork.employeeRepository.GetAsync(x => x.PhoneNumber == dto.PhoneNumber);
+            if (checkEmployeeEmail != null || checkEmployeePhone != null)
+            {
+                return Response<GetEmployeeDto>.Fail($"Employee with email {dto.Email} or phone number {dto.PhoneNumber} already exist", 400);
+            }
             var mappedEntity = _mapper.Map<Employee>(dto);
             var entity = await _unitOfWork.employeeRepository.CreateAsync(mappedEntity);
             if (entity == null)
             {
-                return Response<Employee>.Fail($"Employee {dto.FirstName}, {dto.LastName} was not added", 400);
+                return Response<GetEmployeeDto>.Fail($"Employee {dto.FirstName}, {dto.LastName} was not added", 400);
             }
             await _unitOfWork.SaveChnages();
-            return Response<Employee>.Success($"Employess {entity.FirstName}, " +
-                $"{entity.LastName} is succssfully added", entity, true, 201);
+            return Response<GetEmployeeDto>.Success($"Employess {entity.FirstName}, " +
+                $"{entity.LastName} is succssfully added", _mapper.Map<GetEmployeeDto>(entity), true, 201);
         }
 
-        public async Task<Response<Employee>> DeletEmployeeAsync(Guid id)
+        public async Task<Response<GetEmployeeDto>> DeletEmployeeAsync(Guid id)
         {
             var employee = await _unitOfWork.employeeRepository.DeleteAsync(id);
             if (employee == null)
             {
-                return Response<Employee>.Fail($"Employee with Id {id} was not found", 404);
+                return Response<GetEmployeeDto>.Fail($"Employee with Id {id} was not found", 404);
             }
             await _unitOfWork.SaveChnages();
-            return Response<Employee>.Success($"Employess {employee.FirstName}, " +
-                $"{employee.LastName} is succssfully Deleted", employee, true, 200);
+            return Response<GetEmployeeDto>.Success($"Employess {employee.FirstName}, " +
+                $"{employee.LastName} is succssfully Deleted", _mapper.Map<GetEmployeeDto>(employee), true, 200);
         }
 
-        public async Task<Response<IEnumerable<Employee>>> GetEmployeeAllAsync()
+        public async Task<Response<IEnumerable<GetEmployeeDto>>> GetEmployeeAllAsync()
         {
             var employees = await _unitOfWork.employeeRepository.GetAllAsync();
-            return Response<IEnumerable<Employee>>.Success("Employee list is successfully generated",
-                employees.ToList(), true, 200);
+            return Response<IEnumerable<GetEmployeeDto>>.Success("Employee list is successfully generated",
+                _mapper.Map<List<GetEmployeeDto>>(employees).ToList(), true, 200);
         }
 
-        public async Task<Response<IEnumerable<Employee>>> GetEmployeeAllAsync(Expression<Func<Employee, bool>> predicate,
-            Func<IQueryable<Core.Domain.Employee>, IOrderedQueryable<Core.Domain.Employee>> orderBy = null,
+        public async Task<Response<IEnumerable<GetEmployeeDto>>> GetEmployeeAllAsync(Expression<Func<Employee, bool>> predicate,
+            Func<IQueryable<Employee>, IOrderedQueryable<Employee>> orderBy = null,
             List<string> include = null)
         {
             var employees = await _unitOfWork.employeeRepository.GetAllAsync(predicate, orderBy, include);
-            return Response<IEnumerable<Employee>>.Success("Employee list is successfully generated",
-                employees.ToList(), true, 200);
+            return Response<IEnumerable<GetEmployeeDto>>.Success("Employee list is successfully generated",
+                _mapper.Map<List<GetEmployeeDto>>(employees).ToList(), true, 200);
         }
 
-        public async Task<Response<Employee>> GetEmployeeAsync(Expression<Func<Employee, bool>> exp, List<string> include = null)
+        public async Task<Response<GetEmployeeDto>> GetEmployeeAsync(Expression<Func<Employee, bool>> exp, List<string> include = null)
         {
             var employee = await _unitOfWork.employeeRepository.GetAsync(exp, include);
             if (employee == null)
             {
-                return Response<Employee>.Fail($"Employee that meets conditions{exp} and {include} was not found", 404);
+                return Response<GetEmployeeDto>.Fail($"Employee that meets conditions{exp} and {include} was not found", 404);
             }
-            return Response<Employee>.Success("Employees is found", employee, true, 200);
+            return Response<GetEmployeeDto>.Success("Employees is found", _mapper.Map<GetEmployeeDto>(employee), true, 200);
         }
         public async Task<Response<GetEmployeeDto>> GetEmployeeByEmail(string email)
         {
@@ -91,28 +97,28 @@ namespace ApiWithAuth.Services.EmployeeS
             return Response<GetEmployeeDto>.Success("Employees is found", getEmployee, true, 200);
         }
 
-        public async Task<Response<Employee>> GetEmployeeByIdAsync(Guid id)
+        public async Task<Response<GetEmployeeDto>> GetEmployeeByIdAsync(Guid id)
         {
             var employee = await _unitOfWork.employeeRepository.GetAsync(id);
             if (employee == null)
             {
-                return Response<Employee>.Fail($"Employee with ID {id} was not found", 404);
+                return Response<GetEmployeeDto>.Fail($"Employee with ID {id} was not found", 404);
             }
-            return Response<Employee>.Success("Employees is found", employee, true, 200);
+            return Response<GetEmployeeDto>.Success("Employees is found", _mapper.Map<GetEmployeeDto>(employee), true, 200);
         }
 
 
-        public async Task<Response<Employee>> UpdateEmployeeAsync(Guid id, UpdateEmployeeDto dto)
+        public async Task<Response<GetEmployeeDto>> UpdateEmployeeAsync(Guid id, UpdateEmployeeDto dto)
         {
             var employee = await _unitOfWork.employeeRepository.GetAsync(x => x.Id == id);
             if (employee == null)
             {
-                return Response<Employee>.Fail($"Employee with name {dto.FirstName}, {dto.LastName} and ID {id} was not found", 404);
+                return Response<GetEmployeeDto>.Fail($"Employee with name {dto.FirstName}, {dto.LastName} and ID {id} was not found", 404);
             }
             var mappedEmployee = _mapper.Map<Employee>(dto);
             var upEmployee = await _unitOfWork.employeeRepository.UpdateAsync(mappedEmployee);
             await _unitOfWork.SaveChnages();
-            return Response<Employee>.Success("Employees is found", upEmployee, true, 200);
+            return Response<GetEmployeeDto>.Success("Employees is found", _mapper.Map<GetEmployeeDto>(upEmployee), true, 200);
 
         }
 
